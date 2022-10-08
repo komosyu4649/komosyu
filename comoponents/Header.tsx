a79307820e115de253c77677b243b718ffb73f29
komosyu4649 <komosyu4649@gmail.com>
2022-10-08 23:10:55 +0900

    add: header

--- comoponents/Header.tsx
+++ comoponents/Header.tsx
@@ -1,7 +1,37 @@
 import React from 'react'
 
 const Header = () => {
-  return <div>header</div>
+  const menus = [
+    {
+      item: 'search',
+    },
+    {
+      item: 'works',
+    },
+    {
+      item: 'about',
+    },
+  ]
+  return (
+    <header className="">
+      <div className="">
+        <a href="" className="">
+          komosyu
+        </a>
+        <nav className="">
+          <ul className="">
+            {menus.map((menu) => (
+              <li className="" key={menu.item}>
+                <a href="" className="">
+                  {menu.item}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default Header
