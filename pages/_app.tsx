8dc1cb5141c1b43fd4f79e0a0df05b3d3f274b24
komosyu4649 <komosyu4649@gmail.com>
2022-10-09 12:11:35 +0900

    add: css setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import 'styles/import.scss'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Layout from '../comoponents/Layout'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
