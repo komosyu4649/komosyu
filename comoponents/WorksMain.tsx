f43d15357f61255e207fe13e2271d2686d98fc51
komosyu4649 <komosyu4649@gmail.com>
2022-10-12 23:18:42 +0900

    wip: works

---
+++ comoponents/WorksMain.tsx
@@ -0,0 +1,72 @@
+import Image from 'next/image'
+import style from 'styles/WorksMain.module.scss'
+import styleImg from 'styles/CommonImg.module.scss'
+
+const WorksMain = () => {
+  const worksItems = [
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
+  ]
+  return (
+    <div className={style.container}>
+      <ul className={style.list}>
+        {worksItems.map((worksItem, index) => (
+          <li className={style.item}>
+            <div className={`${styleImg.default} ${style.itemImg}`}>
+              <Image
+                src={worksItem.src}
+                alt=""
+                // layout="fill"
+                layout="responsive"
+                objectFit="cover"
+                // objectFit="contain"
+                width={index === 3 ? '556' : '270'}
+                height={index === 3 ? '473' : '200'}
+              />
+              {/* <img src={worksItem.src} alt="" className="" /> */}
+            </div>
+            <div className={style.itemContent}>
+              <span className={style.itemContentName}>{worksItem.name}</span>
+              <span className={style.itemContentType}>{worksItem.type}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default WorksMain
---
+++ pages/works/index.tsx
@@ -0,0 +1,13 @@
+import Layout from 'comoponents/Layout'
+import WorksMain from 'comoponents/WorksMain'
+import React from 'react'
+
+const Works = () => {
+  return (
+    <Layout>
+      <WorksMain />
+    </Layout>
+  )
+}
+
+export default Works
