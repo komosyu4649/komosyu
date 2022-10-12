654b9ecb9883825abee48f9ed3bda6c93ff9fbdb
komosyu4649 <komosyu4649@gmail.com>
2022-10-12 23:43:03 +0900

    wip: works

--- comoponents/WorksMain.tsx
+++ comoponents/WorksMain.tsx
@@ -39,31 +39,85 @@ const WorksMain = () => {
       name: 'ryoshiro tsuji',
       type: 'webサイト',
     },
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
+    {
+      src: '/asset/img/common/komosyu.jpg',
+      name: 'ryoshiro tsuji',
+      type: 'webサイト',
+    },
   ]
   return (
     <div className={style.container}>
       <ul className={style.list}>
-        {worksItems.map((worksItem, index) => (
-          <li className={style.item}>
-            <div className={`${styleImg.default} ${style.itemImg}`}>
-              <Image
-                src={worksItem.src}
-                alt=""
-                // layout="fill"
-                layout="responsive"
-                objectFit="cover"
-                // objectFit="contain"
-                width={index === 3 ? '556' : '270'}
-                height={index === 3 ? '473' : '200'}
-              />
-              {/* <img src={worksItem.src} alt="" className="" /> */}
-            </div>
-            <div className={style.itemContent}>
-              <span className={style.itemContentName}>{worksItem.name}</span>
-              <span className={style.itemContentType}>{worksItem.type}</span>
-            </div>
-          </li>
-        ))}
+        {worksItems.map(
+          (worksItem, index) => (
+            console.log(index),
+            (
+              <li className={style.item} key={index}>
+                <div className={`${styleImg.default} ${style.itemImg}`}>
+                  <Image
+                    src={worksItem.src}
+                    alt=""
+                    // layout="fill"
+                    layout="responsive"
+                    objectFit="cover"
+                    // objectFit="contain"
+                    width={index % 6 === 3 ? '556' : '270'}
+                    height={index % 6 === 3 ? '473' : '200'}
+                  />
+                  {/* <img src={worksItem.src} alt="" className="" /> */}
+                </div>
+                <div className={style.itemContent}>
+                  <span className={style.itemContentName}>
+                    {worksItem.name}
+                  </span>
+                  <span className={style.itemContentType}>
+                    {worksItem.type}
+                  </span>
+                </div>
+              </li>
+            )
+          )
+        )}
       </ul>
     </div>
   )
