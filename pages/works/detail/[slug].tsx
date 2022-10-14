3c578dce5442e76c687d4db04b2eab3dfcd78500
komosyu4649 <komosyu4649@gmail.com>
2022-10-14 09:57:33 +0900

    wip: works detail

--- pages/works/detail/[slug].tsx
+++ pages/works/detail/[slug].tsx
@@ -1,18 +1,38 @@
-import { GetServerSideProps, GetStaticProps } from 'next'
+import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next'
 import fs from 'fs'
+import matter from 'gray-matter'
+// import * as fs from 'fs'
 
 // const files = fs.readdirSync('data')
 
-const Detail = () => {
+const Detail = ({ posts }) => {
+  console.log(posts)
   return <div>test</div>
 }
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const files = fs.readdirSync('data')
-//   console.log(files)
-//   let work = files.map((workName) => {
-//     const slug = workName
-//   })
-// }
+export const getStaticProps: GetStaticProps = async (params) => {
+  const context = params.params!
+  const file = fs.readFileSync(`posts/${context.slug}.md`, 'utf-8')
+  const { data, content } = matter(file)
+  return {
+    props: {
+      frontMatter: data,
+      content,
+    },
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const files = fs.readdirSync('posts')
+  const paths = files.map((fileName) => ({
+    params: {
+      slug: fileName.replace(/\.md/, ''),
+    },
+  }))
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
 
 export default Detail
