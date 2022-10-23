d1523a13cf1cef4226e63e3b55fbaf8c84f544c3
komosyu4649 <komosyu4649@gmail.com>
2022-10-23 22:04:14 +0900

    update: color

--- comoponents/Head.tsx
+++ comoponents/Head.tsx
@@ -61,7 +61,7 @@ const Meta = ({ title, description }: Props) => {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Noto+Sans+JP:wght@400;500&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap"
         rel="stylesheet"
       />
     </Head>
--- comoponents/NotFoundMain.tsx
+++ comoponents/NotFoundMain.tsx
@@ -1,13 +1,14 @@
 import style from 'styles/NotFoundMain.module.scss'
 import styleButton from 'styles/CommonButton.module.scss'
+import styleFont from 'styles/CommonFont.module.scss'
 import PostLoadMore from './PostLoadMore'
 import Link from 'next/link'
 
 const NotFoundMain = () => {
   return (
     <section className={style.container}>
-      <h1 className={style.title}>404</h1>
-      <p className={style.text}>Page not found</p>
+      <h1 className={`${styleFont.enBold} ${style.title}`}>404</h1>
+      <p className={`${styleFont.enBold} ${style.text}`}>Page not found</p>
       <Link href="/">
         <a className={`${styleButton.lg} ${style.button}`}>ホームに戻る</a>
       </Link>
