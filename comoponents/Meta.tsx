3971a329379aa59b97ef7d8ab54f7685857e4697
komosyu4649 <komosyu4649@gmail.com>
2022-10-28 21:38:16 +0900

    update

--- comoponents/Meta.tsx
+++ comoponents/Meta.tsx
@@ -52,11 +52,10 @@ const Meta = () => {
         content="width=device-width,initial-scale=1.0,maximum-scale=1.0"
       /> */}
       {/*  */}
-      <meta name="application-name" content="PWA App" />
+      <meta name="application-name" content="" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <meta name="apple-mobile-web-app-title" content="PWA App" />
-      <meta name="description" content="Best PWA App in the world" />
+      <meta name="apple-mobile-web-app-title" content="" />
       <meta name="format-detection" content="telephone=no" />
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="msapplication-config" content="/icons/browserconfig.xml" />
@@ -72,18 +71,6 @@ const Meta = () => {
       <link rel="manifest" href="/manifest.json" />
       <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
       <link rel="shortcut icon" href="/favicon.ico" />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:url" content="https://yourdomain.com" />
-      <meta name="twitter:title" content="PWA App" />
-      <meta name="twitter:description" content="Best PWA App in the world" />
-      <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
-      <meta name="twitter:creator" content="@DavidWShadow" />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="PWA App" />
-      <meta property="og:description" content="Best PWA App in the world" />
-      <meta property="og:site_name" content="PWA App" />
-      <meta property="og:url" content="https://yourdomain.com" />
-      <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
       <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
       <link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
       <link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
--- pages/_offline.tsx
+++ pages/_offline.tsx
@@ -1,3 +1,56 @@
-import Page from ".";
+import PostCategories from 'comoponents/PostCategories'
+import PostsList from 'comoponents/PostsList'
+import type { GetStaticProps, NextPage } from 'next'
+import Layout from '../comoponents/Layout'
+import styles from '../styles/Home.module.scss'
+import fs from 'fs'
+import matter from 'gray-matter'
+import { Posts } from 'type'
+import Meta from 'comoponents/Meta'
+import { NextSeo } from 'next-seo'
 
-export default Page;
+const Home: NextPage<Posts> = ({ posts }) => {
+  return (
+    <Layout>
+      <NextSeo title="top title" description="top description" />
+      <PostCategories posts={posts} />
+      <PostsList posts={posts} />
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const files = fs.readdirSync('posts')
+  let posts = files.map((fileName) => {
+    const slug = fileName.replace(/\.md$/, '')
+    const fileContent = fs.readFileSync(`posts/${fileName}`, 'utf-8')
+    const { data } = matter(fileContent)
+    return {
+      frontMatter: data,
+      slug,
+    }
+  })
+
+  posts = posts.sort((a, b) =>
+    new Date(
+      a.frontMatter.modifiedDate
+        ? a.frontMatter.modifiedDate
+        : a.frontMatter.publishedDate
+    ) >
+    new Date(
+      b.frontMatter.modifiedDate
+        ? b.frontMatter.modifiedDate
+        : b.frontMatter.publishedDate
+    )
+      ? -1
+      : 1
+  )
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+
+export default Home
