df6c91445fcb188eb19c49c951173bd55c059d70
komosyu4649 <komosyu4649@gmail.com>
2022-11-03 17:13:07 +0900

    update: type

--- type/index.ts
+++ type/index.ts
@@ -1,101 +1,38 @@
-export type Posts = {
-  posts: [
-    {
-      frontMatter: {
-        public: boolean
-        title: string
-        publishedDate: string
-        modifiedDate: string
-        description: string
-        category: string
-      }
-      slug: string
-    }
-  ]
-}
-
-export type FilteredPosts = {
-  filteredPosts: [
-    {
-      frontMatter: {
-        public: boolean
-        title: string
-        publishedDate: string
-        modifiedDate: string
-        description: string
-        category: string
-      }
-      slug: string
-    }
-  ]
-}
-
-export type Post = {
-  frontMatter: {
-    public: boolean
-    title: string
-    publishedDate: string
-    modifiedDate: string
-    description: string
-    category: string
-  }
-  slug: string
-}
+// post
 
 export type PostFrontMatter = {
-  // frontMatter: {
   public: boolean
   title: string
   publishedDate: string
   modifiedDate: string
   description: string
   category: string
-  // }
+}
+
+export type Post = {
+  frontMatter: PostFrontMatter
+  slug: string
+}
+
+export type Posts = {
+  posts: [
+    Post
+  ]
+}
+
+export type FilteredPosts = {
+  filteredPosts: [
+    Post
+  ]
 }
 
 export type PostContext = {
-  frontMatter: {
-    title: string
-    publishedDate: string
-    modifiedDate: string
-    description: string
-    category: string
-  }
+  frontMatter: PostFrontMatter
   content: string
   allContent: JSX.Element
 }
 
-export type Works = {
-  works: [
-    slug: string,
-    workData: {
-      public: boolean
-      thumbnail: string
-      name: string
-      type: string
-      url: string
-      description: string
-      responsible: string
-      technology: string
-      about: string
-    }
-  ]
-}
-
-export type WorkData = {
-  slug: string
-  workData: {
-    public: boolean
-    thumbnail: string
-    name: string
-    type: string
-    url: string
-    description: string
-    responsible: string
-    technology: string
-    about: string
-  }
-}
+// work
 
 export type WorkDetail = {
   public: boolean
@@ -108,4 +45,16 @@ export type WorkDetail = {
   responsible: string
   technology: string
   about: string
+}
+
+export type Works = {
+  works: [
+    slug: string,
+    workData: WorkDetail
+  ]
+}
+
+export type WorkData = {
+  slug: string
+  workData: WorkDetail
 }
